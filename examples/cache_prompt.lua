4effdf39ff462854191ca86cdaaa0640cc692763
RangerUFO <ufownl@gmail.com>
2024-03-23 17:12:27 +0800

    Only cache the prompts without the content generated by Gemma

--- examples/cache_prompt.lua
+++ examples/cache_prompt.lua
@@ -21,21 +21,11 @@ print("Random seed of session: ", seed)
 print("Reading prompt ...")
 local prompt = io.read("*a")
 local ok, err = session(prompt, function(token, pos, prompt_size)
-  if pos < prompt_size then
-    -- Gemma is processing the prompt
-    io.write(string.format("%d / %d\r", pos + 1, prompt_size))
-  elseif token then
-    -- Stream the token text output by Gemma here
-    if pos == prompt_size then
-      io.write("\n\nreply: ")
-    end
-    io.write(token)
-  else
-    -- Gemma's output reaches the end
-    print()
+  if pos >= prompt_size then
+    return false
   end
+  io.write(string.format("%d / %d\r", pos + 1, prompt_size))
   io.flush()
-  -- return `true` indicates success; return `false` indicates failure and terminates the generation
   return true
 end)
 if not ok then
@@ -50,5 +40,4 @@ if not ok then
   print("Opoos! ", err)
   return
 end
-print()
 print("Done! Session states of the prompt have been dumped to \"dump.bin\"")
