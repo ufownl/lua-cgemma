2359dd32002a1596e5a2f924f2949f1cdb258443
RangerUFO <ufownl@gmail.com>
2024-04-05 21:33:34 +0800

    Adjust print_prompt.lua script according to updated README.md
    Skip the line of badges.

--- demo/print_prompt.lua
+++ demo/print_prompt.lua
@@ -7,6 +7,7 @@ if not readme then
   template:close()
   error(err)
 end
+readme:read("*l")  -- Skip the line of badges
 io.write(string.format(string.sub(template:read("*a"), 1, -2), readme:read("*a")))
 readme:close()
 template:close()
