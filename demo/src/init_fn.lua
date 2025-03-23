d4df216408d5dc73d372faa754295316b9b3b642
RangerUFO <ufownl@gmail.com>
2025-03-24 02:01:36 +0800

    Implement the online demo for function calling of Gemma3

---
+++ demo/src/init_fn.lua
@@ -0,0 +1,21 @@
+function config()
+  return {
+    scheduler = {
+      num_threads = 6,
+      pin = 0
+    },
+    gemma = {
+      tokenizer = "tokenizer.spm",
+      model = "gemma3-12b",
+      weights = "12b-it-sfp.sbs"
+    },
+    session = {
+      temperature = 0.4,
+      top_k = 5
+    },
+    websocket = {
+      max_payload_len = 65536,
+      timeout = 300000
+    }
+  }
+end
