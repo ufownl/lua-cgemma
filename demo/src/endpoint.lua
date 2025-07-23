b9994d02169befd5b5e1d92ea025d79646587bad
RangerUFO <ufownl@gmail.com>
2025-07-23 20:29:29 +0800

    Streamline the implementation of the online demo

--- demo/src/endpoint.lua
+++ demo/src/endpoint.lua
@@ -1,3 +1,13 @@
-local res = gemma_loop()
+local ws, err = require("resty.websocket.server"):new(config().websocket)
+if not ws then
+  ngx.log(ngx.ERR, "websocket error: ", err)
+  return ngx.HTTP_CLOSE
+end
+local ok, err = pcall(gemma_loop, ws)
+ws:send_close()
 collectgarbage()
-ngx.exit(res)
+if not ok then
+  ngx.log(ngx.ERR, err)
+  ngx.exit(ngx.ERROR)
+end
+ngx.exit(ngx.OK)
